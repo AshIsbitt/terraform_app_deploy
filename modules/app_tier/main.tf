6f92596e152908703e38f82154fa16b1285732a8
Ash Isbitt <AIsbitt@tcadmins-air-2.home>
2020-04-29 15:07:03 +0100

    bad gateway

--- modules/app_tier/main.tf
+++ modules/app_tier/main.tf
@@ -167,7 +167,7 @@ resource "aws_security_group" "aws_ash_security_group-dpl" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
